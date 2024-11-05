926e943218a1b221474375bd8d22a9fd9611b35a
eiling chen <chyiling06@gmail.com>
2024-11-05 20:37:34 +0000

    a

--- PriorityQueueLab.java
+++ PriorityQueueLab.java
@@ -1,6 +1,26 @@
-// import statements
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.PriorityQueue;
 
-public class PriorityQueueLab {
-  
+public class PriorityQueueLab{
+    public static void main(String[] args) {
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(); 
+        File file = new File("RandIntegers.txt");
+        try{
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                int i = scanner.nextInt();
+                queue.add(i);
+            }
+            scanner.close();
+        }catch(Exception e){
+            System.out.println("Somthing wrong!");
+        }
+        while(!queue.isEmpty()){      
+            System.out.println(queue);
+            queue.remove();
+        }
+    }
 }
 
